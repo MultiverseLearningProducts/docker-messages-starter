b363f260fe304435e50353116674ca5fbc5889b0
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2023-03-30 14:37:26 +0100

    Update path to database

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 "use strict";
 
-const path = require("path");
 const { DataTypes, Sequelize, ValidationError } = require("sequelize");
 
 const sequelize = new Sequelize({
 	dialect: "sqlite",
 	logging: process.env.NODE_ENV !== "production" && console.log,
-	storage: path.join(__dirname, "db.sqlite"),
+	storage: "/etc/messages/db.sqlite",
 });
 
 const Message = sequelize.define("Message", {
